17adde8d934b241e16069e15bdbb7838996cf0ff
zuhno <zuhno11@gmail.com>
2025-08-12 23:32:53 +0900

    feat: auth base setup in server project chore: eslint import order setting to server project

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+
+import { AppModule } from "./app.module";
 
 const PORT = process.env.PORT ?? 8080;
 
